ee1c6e668dc1ea508987eb0ff7eaf062098d2e31
Natasha <n.chalganova@mail.ru>
2022-11-29 16:50:08 +0300

    Task21

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -10,6 +10,6 @@ double x2 = Convert.ToInt32 (Console.ReadLine());
 double y2 = Convert.ToInt32 (Console.ReadLine());
 double z2 = Convert.ToInt32 (Console.ReadLine());
 double result = Math.Sqrt(Math.Pow ((x2-x1), 2) + Math.Pow ((y2-y1), 2) + Math.Pow ((z2-z1), 2));
-Console.WriteLine(result);
+Console.WriteLine (Math.Round (result, 2));
 
 
